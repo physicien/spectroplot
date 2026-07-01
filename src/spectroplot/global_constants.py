3d6d8fcad1d3fa54806f16224c3342fdf5c1cd4a
Emmanuel Bourret <emmanuel.bourret@umontreal.ca>
2026-07-01 13:06:11 -0400

    FIX: rename x-axis label from Energy to Wavenumber

--- src/spectroplot/global_constants.py
+++ src/spectroplot/global_constants.py
@@ -12,7 +12,7 @@ label_ir = "IR"                     #label ir spectrum plot
 label_raman = "Raman"               #label raman spectrum plot
 label_vpt2 = "VPT2"                 #label vpt2 spectrum plot
 label_vpt2_overt = "Overt. + Comb." #label vpt2 overtones and combination bands
-label_sticks_vib = "Calc. Trans." #label ir/vpt2 stick spectra
+label_sticks_vib = "Calc. Trans."   #label ir/vpt2 stick spectra
 
 show_single_lineshape = False       #show single line shape functions if True
 show_single_lineshape_area = False  #show single line shape areas if True
@@ -32,7 +32,7 @@ y_label = "Absorbance (arb. units)"      #label of the y-axis - ABS (default)
 y_label_PL = "PL Intensity (arb. units)"#label of the y-axis - PL
 y_label_ir = "IR Intensity (arb. units)"#label of the y-axis - IR
 y_label_raman = "Raman Intensity (arb. units)"#label of the y-axis - Raman
-x_label_wn = r'Energy (cm$^{-1}$)'      #label of the x-axis - wave number
+x_label_wn = r'Wavenumber (cm$^{-1}$)'      #label of the x-axis - wave number
 x_label_ev = r'Energy (eV)'             #label of the x-axis - eV
 #x_label_nm = r'$\lambda$ (nm)'          #label of the x-axis - nm
 x_label_nm = r'Wavelength (nm)'         #label of the x-axis - nm
@@ -54,6 +54,8 @@ raman_string = 'RAMAN SPECTRUM'          #start reading orca.out for Raman data
 specstring_start = 'ABSORPTION SPECTRUM VIA TRANSITION ELECTRIC DIPOLE MOMENTS'
 #stop reading orca.out from here
 specstring_end = 'ABSORPTION SPECTRUM VIA TRANSITION VELOCITY DIPOLE MOMENTS'
+#pattern to match ESD root file extension (.spectrum.rootX)
+RE_SPECTRUM_ROOT_PATTERN = r"\.spectrum\.root\d+$"
 w_nm = 20       #w = line width for broadening - nm, FWHM
 w_wn = 1000     #w = line width for broadening - wave numbers, FWHM
 w_ev = 0.1      #w = line width for broadening - eV, FWHM
@@ -63,7 +65,4 @@ npt_nm = 10     #npt = nb of pts per unit to calculate the line shape in nm
 npt_wn = 1      #npt = nb of pts per unit to calculate the line shape in cm**-1
 npt_ev = 1000   #npt = nb of pts per unit to calculate the line shape in eV
 conv_wntoev = 8065.54   #conversion factor from cm**-1 to eV
-
-#pattern to match ESD root file extension (.spectrum.rootX)
-RE_SPECTRUM_ROOT_PATTERN = r"\.spectrum\.root\d+$"
 
